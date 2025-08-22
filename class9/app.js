9a01efdb26783692f49d41a383a377a84f83df8e
Umra-Badar20 <umrabadar555@gmail.com>
2025-08-22 18:08:04 +0500

    Chap 19

---
+++ class9/app.js
@@ -0,0 +1,33 @@
+// alert("Hello");
+
+
+
+    //    i=10 ;  10<=9
+// for(var i = 0; i<=9 ; i++){
+//     console.log("Hello");
+// }
+
+
+var arr= ['Khi','Isl','lhr','swt','hyd'];
+
+// console.log("Welcome to ",arr[0]);
+// console.log("Welcome to ",arr[1]);
+// console.log("Welcome to ",arr[2]);
+// console.log("Welcome to ",arr[3]);
+//___________________________Break___________________________
+// for(var i=0; i <arr.length; i++){
+//     if(arr[i]==='Isl'){
+//         console.log("City Found"); 
+//         break;           
+//     }else{
+//         console.log("City not found");
+//     }
+// }
+for(var i=0; i <arr.length; i++){
+    if(arr[i]==='Isl'){
+        console.log("City Found"); 
+        break;           
+    }else{
+        console.log("City not found");
+    }
+}
