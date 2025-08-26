789a5ac113c9f654c2a3c56d85f03f2ac9474b55
Umra-Badar20 <umrabadar555@gmail.com>
2025-08-26 18:00:30 +0500

    Chap 19

---
+++ class10/app.js
@@ -0,0 +1,29 @@
+// var arr = ["Karachi","cheyenne", "Santa Fe", "tucson", "Great Falls", "Honolulu"];
+// var userCity =prompt("Enter your city name:")
+// flag = false
+// for(var i = 0; i<arr.length; i++){
+   
+//     if(arr[i].toLowerCase()===userCity.toLowerCase()){
+//         console.log("City found at index "+ [i]); 
+//         flag = true
+//         break;
+//     }
+// }
+                                                                                                              
+// if(flag === false){
+//     console.log("City not Found")
+// }
+
+
+var arr = [24, 53,4, 78, 91, 12,3]
+var min = arr[0]
+// console.log(min);
+
+for(var i=0; i< arr.length; i++){
+       3 < 4
+    if(arr[i]<min){
+        min = arr[i]
+    }
+}
+console.log(min);
+
