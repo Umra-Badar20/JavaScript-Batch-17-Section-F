56daedb9cb6d91404fdd6109b2d059db577a6df9
Umra-Badar20 <umrabadar555@gmail.com>
2025-08-28 17:23:38 +0500

    Chap 19

--- class10/app.js
+++ class10/app.js
@@ -1,18 +1,18 @@
-// var arr = ["Karachi","cheyenne", "Santa Fe", "tucson", "Great Falls", "Honolulu"];
-// var userCity =prompt("Enter your city name:")
-// flag = false
-// for(var i = 0; i<arr.length; i++){
+var arr = ["Karachi","cheyenne", "Santa Fe", "tucson", "Great Falls", "Honolulu"];
+var userCity =prompt("Enter your city name:")
+var flag = false
+for(var i = 0; i<arr.length; i++){
    
-//     if(arr[i].toLowerCase()===userCity.toLowerCase()){
-//         console.log("City found at index "+ [i]); 
-//         flag = true
-//         break;
-//     }
-// }
+    if(arr[i].toLowerCase()===userCity.toLowerCase()){
+        console.log("City found at index "+ [i]); 
+        flag = true
+        break;
+    }
+}
                                                                                                               
-// if(flag === false){
-//     console.log("City not Found")
-// }
+if(flag === false){
+    console.log("City not Found")
+}
 
 
 var arr = [24, 53,4, 78, 91, 12,3]
---
+++ class11/app.js
@@ -0,0 +1,33 @@
+// for (var i = 0; i < 5; i++) { //1
+//     console.log("Entering inner loop " + i + " Times");
+//     for (var j = 0; j < 10; j++) {//2
+//         console.log(i, j);
+//     }
+// }
+//0,0
+//0,1
+//0,2
+//0,3
+//0,4
+//1,0
+//1,1
+//1,2
+var arr = [
+    ["Umra", "Badar", "SMIT"],
+    ["Samen", "Arshad"],
+    ["Rida", "Abid"]
+]
+for(var i =0; i<arr.length;i++){//2
+    // console.log(arr[i]);  
+    for(var j=0; j<arr[i].length; j++){//1
+        console.log(arr[i][j]);
+        
+    }
+}
+arr[0] =["Umra", "Badar"] 
+arr[0][0]= "Umra"
+arr[0][1] ="Badar"
+
+arr[1] = ["Samen", "Arshad"]
+arr[1][0]= "Samen"
+arr[1][1]= "Arshad"
