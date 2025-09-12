c75637313015c8174fa350a3055fca2fedee3f1b
Umra-Badar20 <umrabadar555@gmail.com>
2025-09-12 18:02:43 +0500

    Palindrome & Double space checker

--- class16/app.js
+++ class16/app.js
@@ -1,20 +1,49 @@
-var word = prompt("Enter a word:") // ciiviic
-var isPalindrome = false;
+// var word = prompt("Enter a word:") // ciiviic
+// var isPalindrome = false;
 
-for(var i = 0; i<(word.length-1)/2; i++){
-    // console.log(word[i]);
-    // console.log(word.charAt(word.length-1-i));
-    var startingLetters = word[i]
-    var endingLetters = word.charAt(word.length-1-i)
+// for (var i = 0; i < (word.length - 1) / 2; i++) {
+//     // console.log(word[i]);
+//     // console.log(word.charAt(word.length-1-i));
+//     var startingLetters = word[i]
+//     var endingLetters = word.charAt(word.length - 1 - i)
 
-    if(startingLetters===endingLetters){
-        isPalindrome=true
-    }
+//     if (startingLetters === endingLetters) {
+//         isPalindrome = true
+//     }
+// }
+// if (isPalindrome) {
+//     console.log("It's a Palindrome!");
+
+// } else {
+//     console.log("It's not a Palindrome!");
+
+// }
+
+// var str = prompt("Enter a string:")
+// for(var i=0; i<str.length; i++){
+//     if(str.slice(i , i+2)==="  "){
+//         alert("No double spaces allowed!")
+//         break;
+//     }
+// }
+
+
+var userWord = prompt("enter any word") // sameen
+var result = "";
+
+for (var i = userWord.length -1 ; i >= 0 ; i--) {
+//  console.log(userWord[i]);
+result += userWord[i]
+
+
 }
-if(isPalindrome){
-    console.log("It's a Palindrome!");
+if (userWord === result) {
+    console.log("its a palindrome");
+    
     
 }else{
-    console.log("It's not a Palindrome!");
-
+    console.log("its not palindrome");
+    
 }
+// console.log(result)
+
