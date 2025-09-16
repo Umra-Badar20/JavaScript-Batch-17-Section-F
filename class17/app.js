6d365c4591ac3a2ac49624dbad36fa897f5ce22d
Umra-Badar20 <umrabdar555@gmail.com>
2025-09-16 17:55:37 +0500

    Chap 27

---
+++ class17/app.js
@@ -0,0 +1,26 @@
+// var str = "Pakistan,[e] officially the Islamic Republic of Pakistan,[f] is a country in South Asia. It is the fifth-most populous country, with a population of over 241.5 million,[c] having the second-largest Muslim population as of 2023. Islamabad is the nation's capital, while Karachi is its largest city and financial centre. Pakistan is the 33rd-largest country by area. Bounded by the Arabian Sea on the south, the Gulf of Oman on the southwest, and the Sir Creek on the southeast, it shares land borders with India to the east; Afghanistan to the west; Iran to the southwest; and China to the northeast. It shares a maritime border with Oman in the Gulf of Oman, and is separated from Tajikistan in the northwest by Afghanistan's narrow Wakhan Corridor."
+// // var updStr=  str.replaceAll("Pakistan","Afghanistan")
+// // var updStr=  str.replace("Pakistan","Afghanistan")
+// var updStr=  str.replace(/Pakistan/g,"Afghanistan")
+// document.write(updStr)
+
+
+// var num = 2.9999 
+// console.log(Math.round(num)); //2.4 = 2
+// console.log(Math.ceil(num));  //2.4 = 3
+// console.log(Math.floor(num));  //2.9 = 2
+
+// var randomNumber = Math.random() *10;
+// console.log(Math.round(randomNumber));
+
+// console.log(Math.abs(-15));
+// var abs = +prompt("Enter a number:")
+// if(abs<0){
+//     console.log("Absoulte of ",abs ," is ",-abs); 
+// }else{
+//     console.log("Absoulte of ",abs ," is ",abs); 
+// }
+
+// console.log(isNaN("2")); // false bcz it can be converted into a number
+// console.log(isNaN(2)); // false bcz it is a number
+// console.log(isNaN("Umra")); // true bcz it can not be converted into a number
