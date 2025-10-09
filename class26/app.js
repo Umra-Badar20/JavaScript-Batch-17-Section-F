ba00c000eab92c92ef6ae940464048385226dcc7
Umra-Badar20 <umrabadar555@gmail.com>
2025-10-09 17:59:06 +0500

    Chap 42

---
+++ class26/app.js
@@ -0,0 +1,33 @@
+// Swal.fire("SweetAlert2 is working!");
+// Swal.fire({
+//   title: "I am Umra?",
+//   text: "You won't be able to revert this!",
+//   icon: "success",
+//   showCancelButton: true,
+//   confirmButtonColor: "#41d630ff",
+//   cancelButtonColor: "rgba(84, 16, 16, 1)",
+//   confirmButtonText: "Yes, delete it!"
+// }).then((result) => {
+//   if (result.isConfirmed) {
+//     Swal.fire({
+//       title: "Deleted!",
+//       text: "Your file has been deleted.",
+//       icon: "success"
+//     });
+//   }
+// });
+
+
+
+// var i =0;
+// while(i<10){
+//     console.log(i);
+//     i++    
+// }
+
+var i = 0;
+do{
+    console.log(i);
+    i++
+    
+}while(i<0)
