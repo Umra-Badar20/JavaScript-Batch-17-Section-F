aab9337059b45bf4985b5f407657ea62a1dce5f1
Umra-Badar20 <umrabadar555@gmail.com>
2025-10-30 18:02:37 +0500

    Chap 61 half

--- class32/app.js
+++ class32/app.js
@@ -1,20 +1,24 @@
-// var para =document.getElementsByClassName("para")[0]
-var div = document.getElementById("main")
-console.log(main);
+// // var para =document.getElementsByClassName("para")[0]
+// var div = document.getElementById("main")
+// console.log(main);
 
-var para =div.getElementsByTagName("p")//[p,p,p]
-console.log(para);
+// var para =div.getElementsByTagName("p")//[p,p,p]
+// console.log(para);
 
-function zoom() {
-    // para.className += " zoom"
-    for(var i=0;i<para.length;i++){
-        para[i].classList.add("zoom")
-    }
-}
-function hide() {
-    para.className += " hidden"
-}
-function left(){
-    para.style.cssFloat = "left"
-}
-console.log(document.childNodes[1].childNodes[2].childNodes[1].childNodes[3].parentNode);
+// function zoom() {
+//     // para.className += " zoom"
+//     for(var i=0;i<para.length;i++){
+//         para[i].classList.add("zoom")
+//     }
+// }
+// function hide() {
+//     para.className += " hidden"
+// }
+// function left(){
+//     para.style.cssFloat = "left"
+// }
+// console.log(document.childNodes[1].childNodes[2].childNodes[1].childNodes[3].parentNode);
+var fname="Umra"
+var lname ="Badar"
+var fullname = `<h1> My name is ${fname} ${lname}</h1>`
+document.write(fullname);
