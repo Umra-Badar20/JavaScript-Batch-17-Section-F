9f66c716ea9c2c2f42c96ee43cb20a8f368a85b3
Umra-Badar20 <umrabadar555@gmail.com>
2025-11-07 18:02:58 +0500

    Revision of chap 60

---
+++ class33/app.js
@@ -0,0 +1,19 @@
+var a = 10
+function num(){
+    console.log(a);
+    var a = 20;
+
+}
+num()//
+
+// function num(){
+//     var a;
+//     console.log(a);
+//     a=20   
+// }
+function addName(){
+    document.getElementById("name").value= "Umra"
+    console.log(name);   
+
+    
+}
